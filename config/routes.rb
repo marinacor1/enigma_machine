12d54dee2599779458817dab95ac7cf675d56171
marinacor1 <marinaspam1234@gmail.com>
2016-08-22 15:27:54 -0600

    Message is not getting passed through

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/encrypt', to: 'encryption#new'
-  post '/encrypt', to: 'encryption#create'
+  get '/encrypt/new', to: 'encryption#new'
+  post '/encrypt/', to: 'encryption#create'
   get '/encrypt/:id', to: 'encryption#show', as: "encryption"
   post '/decrypt', to: 'encryption#decrypt'
 
