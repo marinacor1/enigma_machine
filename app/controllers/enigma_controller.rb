968d91f125c9f14e0019b7ed3d76d1eac9c15f85
marinacor1 <marinaspam1234@gmail.com>
2016-08-21 20:34:11 -0600

    First three pages created

---
+++ app/controllers/enigma_controller.rb
@@ -0,0 +1,10 @@
+class EnigmaController < ApplicationController
+
+  def encrypt
+
+  end
+
+  def decrypt
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get '/encrypt', 'enigma#encrypt'
+  get '/decrypt', 'enigma#decrypt'
+
 end
