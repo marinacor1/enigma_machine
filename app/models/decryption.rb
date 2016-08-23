705f450f663489ea2f0f6abc7a154091bd5520fc
marinacor1 <marinaspam1234@gmail.com>
2016-08-22 22:51:39 -0600

    Add decrypt

--- app/models/decryption.rb
+++ app/models/decryption.rb
@@ -1,2 +1,9 @@
 class Decryption < ActiveRecord::Base
+
+  def decrypt_now
+    e = Enigma.new
+    key = e.key.key
+    my_message = self.message
+    e.decrypt(my_message, key, e.offsets_info)
+  end
 end
